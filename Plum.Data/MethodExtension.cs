83d12e5a0df5d02ef97786f1dcc5944d0649c649
admin <admin@DESKTOP-OQ2558K>
2017-09-24 23:25:12 +0800

    完善MethodExtension中的DataTable转List实体方法，解决可为空类型报错的BUG。

--- Plum.Data/MethodExtension.cs
+++ Plum.Data/MethodExtension.cs
@@ -41,6 +41,22 @@ namespace Vic.Data
         {
             private static readonly MethodInfo getValueMethod = typeof(DataRow).GetMethod("get_Item", new Type[] { typeof(int) });
             private static readonly MethodInfo isDBNullMethod = typeof(DataRow).GetMethod("IsNull", new Type[] { typeof(int) });
+            private static readonly MethodInfo Object_ToString = typeof(object).GetMethod("ToString");
+            //private static readonly MethodInfo Convert_IsDBNull = typeof(DBConvert).GetMethod("IsDBNull", new Type[] { typeof(object) });
+            private static readonly MethodInfo Convert_ToInt16 = typeof(DBConvert).GetMethod("ToInt16", new Type[] { typeof(object) });
+            private static readonly MethodInfo Convert_ToInt32 = typeof(DBConvert).GetMethod("ToInt32", new Type[] { typeof(object) });
+            private static readonly MethodInfo Convert_ToInt64 = typeof(DBConvert).GetMethod("ToInt64", new Type[] { typeof(object) });
+            private static readonly MethodInfo Convert_ToBoolean = typeof(DBConvert).GetMethod("ToBoolean", new Type[] { typeof(object) });
+            private static readonly MethodInfo Convert_ToDateTime = typeof(DBConvert).GetMethod("ToDateTime", new Type[] { typeof(object) });
+            private static readonly MethodInfo Convert_ToDecimal = typeof(DBConvert).GetMethod("ToDecimal", new Type[] { typeof(object) });
+            private static readonly MethodInfo Convert_ToDouble = typeof(DBConvert).GetMethod("ToDouble", new Type[] { typeof(object) });
+            private static readonly MethodInfo Convert_ToNullInt16 = typeof(DBConvert).GetMethod("ToNullableInt16", new Type[] { typeof(object) });
+            private static readonly MethodInfo Convert_ToNullInt32 = typeof(DBConvert).GetMethod("ToNullableInt32", new Type[] { typeof(object) });
+            private static readonly MethodInfo Convert_ToNullInt64 = typeof(DBConvert).GetMethod("ToNullableInt64", new Type[] { typeof(object) });
+            private static readonly MethodInfo Convert_ToNullBoolean = typeof(DBConvert).GetMethod("ToNullableBoolean", new Type[] { typeof(object) });
+            private static readonly MethodInfo Convert_ToNullDateTime = typeof(DBConvert).GetMethod("ToNullableDateTime", new Type[] { typeof(object) });
+            private static readonly MethodInfo Convert_ToNullDecimal = typeof(DBConvert).GetMethod("ToNullableDecimal", new Type[] { typeof(object) });
+            private static readonly MethodInfo Convert_ToNullDouble = typeof(DBConvert).GetMethod("ToNullableDouble", new Type[] { typeof(object) });
             private delegate Entity Load(DataRow dataRecord);
             private Load handler;
             private DataTableEntityBuilder() { }
@@ -57,6 +73,15 @@ namespace Vic.Data
             {
                 DataTableEntityBuilder<Entity> dynamicBuilder = new DataTableEntityBuilder<Entity>();
                 DynamicMethod method = new DynamicMethod("DynamicCreateEntity", typeof(Entity), new Type[] { typeof(DataRow) }, typeof(Entity), true);
+
+                #region 测试用，生成dll文件
+                //AssemblyName assemblyName = new AssemblyName("test");
+                //AssemblyBuilder assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.RunAndSave);
+                //ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule("testModule", "test.dll");
+                //TypeBuilder typeBuilder = moduleBuilder.DefineType("test", TypeAttributes.Public);
+                //MethodBuilder method = typeBuilder.DefineMethod("SetModel", MethodAttributes.Public, typeof(Entity), new Type[] { typeof(DataRow), typeof(Entity) });
+                #endregion
+
                 ILGenerator generator = method.GetILGenerator();
                 LocalBuilder result = generator.DeclareLocal(typeof(Entity));
                 generator.Emit(OpCodes.Newobj, typeof(Entity).GetConstructor(Type.EmptyTypes));
@@ -67,23 +92,365 @@ namespace Vic.Data
                     Label endIfLabel = generator.DefineLabel();
                     if (propertyInfo != null && propertyInfo.GetSetMethod() != null)
                     {
+                        Type memberType = propertyInfo.PropertyType;
+                        Type nullUnderlyingType = Nullable.GetUnderlyingType(memberType);
+                        Type unboxType = nullUnderlyingType != null ? nullUnderlyingType : memberType;
+
                         generator.Emit(OpCodes.Ldarg_0);
                         generator.Emit(OpCodes.Ldc_I4, i);
                         generator.Emit(OpCodes.Callvirt, isDBNullMethod);
                         generator.Emit(OpCodes.Brtrue, endIfLabel);
                         generator.Emit(OpCodes.Ldloc, result);
+
                         generator.Emit(OpCodes.Ldarg_0);
                         generator.Emit(OpCodes.Ldc_I4, i);
                         generator.Emit(OpCodes.Callvirt, getValueMethod);
-                        generator.Emit(OpCodes.Unbox_Any, propertyInfo.PropertyType);
+
+                        //if (unboxType == typeof(byte[]) || unboxType == typeof(string))
+                        //{
+                        //    generator.Emit(OpCodes.Castclass, memberType);
+                        //}
+                        //else
+                        //{
+                        //    generator.Emit(OpCodes.Unbox_Any, unboxType);
+                        //    if (nullUnderlyingType != null)
+                        //    {
+                        //        generator.Emit(OpCodes.Newobj, memberType.GetConstructor(new[] { nullUnderlyingType }));
+                        //    }
+                        //}
+                        if (unboxType == typeof(byte[]))
+                        {
+                            generator.Emit(OpCodes.Castclass, memberType);
+                        }
+                        else if (unboxType == typeof(string))
+                        {
+                            generator.Emit(OpCodes.Callvirt, Object_ToString);
+                        }
+                        else
+                        {
+                            ConvertValueType(generator, propertyInfo);
+                        }
+
+                        //generator.Emit(OpCodes.Unbox_Any, propertyInfo.PropertyType);
                         generator.Emit(OpCodes.Callvirt, propertyInfo.GetSetMethod());
                         generator.MarkLabel(endIfLabel);
                     }
                 }
                 generator.Emit(OpCodes.Ldloc, result);
                 generator.Emit(OpCodes.Ret);
+
+                #region 测试用，生成dll文件
+                //Type t = typeBuilder.CreateType();
+                //assemblyBuilder.Save("test.dll");
+                #endregion
+
                 dynamicBuilder.handler = (Load)method.CreateDelegate(typeof(Load));
                 return dynamicBuilder;
+            }
+
+            /// <summary>  
+            /// 由T的属性类型决定使用的Convert方法。  
+            /// </summary>  
+            /// <param name="ilg"></param>  
+            /// <param name="pi"></param>  
+            /// <returns></returns>
+            private static void ConvertValueType(ILGenerator ilg, PropertyInfo pi)
+            {
+                //不可空类型  
+                TypeCode code = Type.GetTypeCode(pi.PropertyType);
+                switch (code)
+                {
+                    case TypeCode.Int16:
+                        ilg.Emit(OpCodes.Call, Convert_ToInt16);
+                        return;
+                    case TypeCode.Int32:
+                        ilg.Emit(OpCodes.Call, Convert_ToInt32);
+                        return;
+                    case TypeCode.Int64:
+                        ilg.Emit(OpCodes.Call, Convert_ToInt64);
+                        return;
+                    case TypeCode.Boolean:
+                        ilg.Emit(OpCodes.Call, Convert_ToBoolean);
+                        return;
+                    case TypeCode.String:
+                        ilg.Emit(OpCodes.Callvirt, Object_ToString);
+                        return;
+                    case TypeCode.DateTime:
+                        ilg.Emit(OpCodes.Call, Convert_ToDateTime);
+                        return;
+                    case TypeCode.Decimal:
+                        ilg.Emit(OpCodes.Call, Convert_ToDecimal);
+                        return;
+                    case TypeCode.Double:
+                        ilg.Emit(OpCodes.Call, Convert_ToDouble);
+                        return;
+                }
+                //可空类型处理  
+                Type type = Nullable.GetUnderlyingType(pi.PropertyType);
+                if (type != null)
+                {
+                    code = Type.GetTypeCode(type);
+                    switch (code)
+                    {
+                        case TypeCode.Int16:
+                            ilg.Emit(OpCodes.Call, Convert_ToNullInt16);
+                            return;
+                        case TypeCode.Int32:
+                            ilg.Emit(OpCodes.Call, Convert_ToNullInt32);
+                            return;
+                        case TypeCode.Int64:
+                            ilg.Emit(OpCodes.Call, Convert_ToNullInt64);
+                            return;
+                        case TypeCode.Boolean:
+                            ilg.Emit(OpCodes.Call, Convert_ToNullBoolean);
+                            return;
+                        case TypeCode.DateTime:
+                            ilg.Emit(OpCodes.Call, Convert_ToNullDateTime);
+                            return;
+                        case TypeCode.Decimal:
+                            ilg.Emit(OpCodes.Call, Convert_ToNullDecimal);
+                            return;
+                        case TypeCode.Double:
+                            ilg.Emit(OpCodes.Call, Convert_ToNullDouble);
+                            return;
+                    }
+                }
+                throw new Exception(string.Format("不支持\"{0}\"类型的转换！", pi.PropertyType.Name));
+            }
+
+        }
+
+        /// <summary>  
+        /// 数据库类型的值到本地类型的转换类  
+        /// </summary>  
+        public static class DBConvert
+        {
+            public static bool IsDBNull(object value)
+            {
+                return object.Equals(DBNull.Value, value);
+            }
+            public static short ToInt16(object value)
+            {
+                if (value is short)
+                {
+                    return (short)value;
+                }
+                try
+                {
+                    return Convert.ToInt16(value);
+                }
+                catch
+                {
+                    return 0;
+                }
+            }
+            public static int ToInt32(object value)
+            {
+                if (value is int)
+                {
+                    return (int)value;
+                }
+                try
+                {
+                    return Convert.ToInt32(value);
+                }
+                catch
+                {
+                    return 0;
+                }
+            }
+            public static long ToInt64(object value)
+            {
+                if (value is long)
+                {
+                    return (long)value;
+                }
+                try
+                {
+                    return Convert.ToInt64(value);
+                }
+                catch
+                {
+                    return 0;
+                }
+            }
+            public static bool ToBoolean(object value)
+            {
+                if (value == null)
+                {
+                    return false;
+                }
+                if (value is bool)
+                {
+                    return (bool)value;
+                }
+                if (value.Equals("1") || value.Equals("-1"))
+                {
+                    value = "true";
+                }
+                else if (value.Equals("0"))
+                {
+                    value = "false";
+                }
+
+                try
+                {
+                    return Convert.ToBoolean(value);
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            public static DateTime ToDateTime(object value)
+            {
+                if (value is DateTime)
+                {
+                    return (DateTime)value;
+                }
+                try
+                {
+                    return Convert.ToDateTime(value);
+                }
+                catch
+                {
+                    return DateTime.MinValue;
+                }
+            }
+            public static decimal ToDecimal(object value)
+            {
+                if (value is decimal)
+                {
+                    return (decimal)value;
+                }
+                try
+                {
+                    return Convert.ToDecimal(value);
+                }
+                catch
+                {
+                    return 0;
+                }
+            }
+            public static double ToDouble(object value)
+            {
+                if (value is double)
+                {
+                    return (double)value;
+                }
+                try
+                {
+                    return Convert.ToDouble(value);
+                }
+                catch
+                {
+                    return 0;
+                }
+            }
+            public static Nullable<short> ToNullableInt16(object value)
+            {
+                if (value is short)
+                {
+                    return new Nullable<short>((short)value);
+                }
+                try
+                {
+                    return new Nullable<short>(Convert.ToInt16(value));
+                }
+                catch
+                {
+                    return new Nullable<short>();
+                }
+            }
+            public static Nullable<int> ToNullableInt32(object value)
+            {
+                if (value is int)
+                {
+                    return new Nullable<int>((int)value);
+                }
+                try
+                {
+                    return new Nullable<int>(Convert.ToInt32(value));
+                }
+                catch
+                {
+                    return new Nullable<int>();
+                }
+            }
+            public static Nullable<long> ToNullableInt64(object value)
+            {
+                if (value is long)
+                {
+                    return new Nullable<long>((long)value);
+                }
+                try
+                {
+                    return new Nullable<long>(Convert.ToInt64(value));
+                }
+                catch
+                {
+                    return new Nullable<long>();
+                }
+            }
+            public static Nullable<bool> ToNullableBoolean(object value)
+            {
+                if (value is bool)
+                {
+                    return new Nullable<bool>((bool)value);
+                }
+                try
+                {
+                    return new Nullable<bool>(Convert.ToBoolean(value));
+                }
+                catch
+                {
+                    return new Nullable<bool>();
+                }
+            }
+            public static Nullable<DateTime> ToNullableDateTime(object value)
+            {
+                if (value is DateTime)
+                {
+                    return new Nullable<DateTime>((DateTime)value);
+                }
+                try
+                {
+                    return new Nullable<DateTime>(Convert.ToDateTime(value));
+                }
+                catch
+                {
+                    return new Nullable<DateTime>();
+                }
+            }
+            public static Nullable<decimal> ToNullableDecimal(object value)
+            {
+                if (value is decimal)
+                {
+                    return new Nullable<decimal>((decimal)value);
+                }
+                try
+                {
+                    return new Nullable<decimal>(Convert.ToDecimal(value));
+                }
+                catch
+                {
+                    return new Nullable<decimal>();
+                }
+            }
+            public static Nullable<double> ToNullableDouble(object value)
+            {
+                if (value is double)
+                {
+                    return new Nullable<double>((double)value);
+                }
+                try
+                {
+                    return new Nullable<double>(Convert.ToDouble(value));
+                }
+                catch
+                {
+                    return new Nullable<double>();
+                }
             }
         }
 
